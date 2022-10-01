21cadbf8453282fc1312a981cf091355b4484f32
kukanji <111495470+kukanji@users.noreply.github.com>
2022-10-01 19:46:49 +0900

    nonmaximum_suppress関数の追加

--- ssd.py
+++ ssd.py
@@ -160,3 +160,77 @@ class DBox(object):
 
         return output
         
+
+def decode(loc, dbox_list):
+
+    boxes = torch.cat((
+        dbox_list[:, :2] + loc[:, :2] * 0.1 * dbox_list[:, 2:],
+        dbox_list[:, 2:] * torch.exp(loc[:, 2:] * 0.2)
+        ), dim = 1)
+
+    boxes[:, :2] -= boxes[:, 2:] / 2
+    boxes[:, 2:] += boxes[:, :2]
+
+    return boxes
+
+
+def nonmaximum_suppress(boxes, scores, overlap = 0.5, top_k = 200):
+
+    count = 0
+    keep = scores.new(scores.size(0)).zero_().long()
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+    area = torch.mul(x2 - x1, y2 - y1)
+
+    tmp_x1 = boxes.new()
+    tmp_y1 = boxes.new()
+    tmp_x2 = boxes.new()
+    tmp_y2 = boxes.new()
+    tmp_w = boxes.new()
+    tmp_h = boxes.new()
+
+    v, idx = scores.sort(0)
+
+    idx = idx[-top_k:]
+
+    while idx.numel() > 0:
+        i = idx[-1]
+
+        keep[count] = i
+        count += 1
+        
+        if idx.size(0) == 1:
+            break
+
+        idx = idx[:-1]
+
+        torch.index_select(x1, 0, idx, out = tmp_x1)
+        torch.index_select(y1, 0, idx, out = tmp_y1)
+        torch.index_select(x2, 0, idx, out = tmp_x2)
+        torch.index_select(y2, 0, idx, out = tmp_y2)
+
+        tmp_x1 = torch.clamp(tmp_x1, min = x1[i])
+        tmp_y1 = torch.clamp(tmp_y1, min = y1[i])
+        tmp_x2 = torch.clamp(tmp_x2, min = x2[i])
+        tmp_y2 = torch.clamp(tmp_y2, min = y2[i])
+
+        tmp_w.resize_as_(tmp_x2)
+        tmp_h.resize_as_(tmp_y2)
+
+        tmp_w = tmp_x2 - tmp_x1
+        tmp_h = tmp_y2 - tmp_y1
+        
+        tmp_w = torch.clamp(tmp_w, min = 0.0)
+        tmp_h = torch.clamp(tmp_h, min = 0.0)
+
+        inter = tmp_w * tmp_h
+
+        rem_areas = torch.index_select(area, 0, idx)
+        union = (rem_areas - inter) + area[i]
+        IoU = inter / union
+
+        idx = idx[IoU.le(overlap)]
+
+    return keep, count
