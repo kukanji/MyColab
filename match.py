32b35d88da0b1ae63d2c379493c02c2dfd87d14b
kukanji <111495470+kukanji@users.noreply.github.com>
2022-10-09 20:55:24 +0900

    intersect関数の作成

--- match.py
+++ match.py
@@ -4,4 +4,22 @@ def point_form(boxes):
 
     return torch.cat((boxes[:, :2] - boxes[:, 2:] / 2, boxes[:, :2] + boxes[:, 2:] / 2, 1))
 
+#2個のボックスが重なる面積を求める関数
+def intersect(box_a, box_b):
+    A = box_a.size(0)
+    B = box_b.size(0)
+
+    max_xy = torch.min(
+        box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
+        box_b[:, 2:].unsqueeze(0).expand(A, B, 2)
+    )
+
+    min_xy = torch.max(
+        box_a[:, :2].unsqueeze(1).expand(A, B, 2),
+        box_b[:, :2].unsqueeze(0).expand(A, B, 2)
+    )
+
+    inter = torch.clamp((max_xy - min_xy), min = 0)
+
+    return inter[:, :, 0] * inter[:, :, 1]
     
