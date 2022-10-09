3636a9f40086d273c88aadb3712a9847e5f0798d
kukanji <111495470+kukanji@users.noreply.github.com>
2022-10-09 21:27:59 +0900

    jaccard係数(IoU)を求める関数の作成

--- match.py
+++ match.py
@@ -22,4 +22,11 @@ def intersect(box_a, box_b):
     inter = torch.clamp((max_xy - min_xy), min = 0)
 
     return inter[:, :, 0] * inter[:, :, 1]
-    
+
+#jaccard係数(IoU)を求める関数
+def jaccard(box_a, box_b):
+    inter = intersect(box_a, box_b)
+    area_a = ((box_a[:, 2] - box_a[:, 0]) * (box_a[:, 3] - box_a[:, 1])).unsqueeze(1).expand_as(inter)
+    area_b = ((box_b[:, 2] - box_b[:, 0]) * (box_b[:, 3] - box_b[:, 1])).unsqueeze(0).expand_as(inter)
+    union = area_a + area_b - inter
+    return inter / union
