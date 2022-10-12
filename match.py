4c3e13a9544d1dfa9270e7dde3e893cd4fcb44bb
kukanji <111495470+kukanji@users.noreply.github.com>
2022-10-12 19:40:24 +0900

    DBoxのオフセット情報を作る関数の作成

--- match.py
+++ match.py
@@ -59,3 +59,13 @@ def match(threshold, truths, priors, variances, labels, loc_t, conf_t, idx):
 
     loc_t[idx] = loc
     conf_t[idx] = conf
+
+#DBoxのオフセット情報を作る関数
+def encode(matched, priors, variances):
+    g_cxcy = (matched[:, :2] + matched[:, 2:]) / 2 - priors[:, :2]
+    g_cxcy /= (variances[0] * priors[:, 2:])
+
+    g_wh = (matched[:, 2:] - matched[:, :2]) / priors[:, 2:]
+    g_wh = torch.log(g_wh) / variances[1]
+
+    return torch.cat([g_cxcy, g_wh], 1)
